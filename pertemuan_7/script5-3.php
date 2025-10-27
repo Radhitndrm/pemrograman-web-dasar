5ab72eac45929f4ddda127d0c065bbc0774d66e1
Radhitndrm <radhit.andromeda@gmail.com>
2025-10-27 17:05:53 +0800

    update

--- pertemuan_7/script5-3.php
+++ pertemuan_7/script5-3.php
@@ -1,17 +1,23 @@
 <html>
 
 <head>
-  <title>Konversi Waktu Tempah ke Detik</title>
+  <title>Konversi Waktu Tempuh ke Detik</title>
 </head>
-<h1>Konversi Waktu Tempuh ke Detik</h1>
-<?php
-$jam = 10;
-$menit = 16;
-$detik = 42;
-$jamKeDetik = $menit * 60;
-$detikKeDetik = $detik;
-$totalDetik = $jamKeDetik + $menitKeDetik = $detikKeDetik;
-echo "<p> Jika Waktu " . $jam . ":" . $menit . ":" . $detik . " dinyatakan dalam satuan detik adalah : " . $totalDetik . "</p>";
-?>
+
+<body>
+  <h1>Konversi Waktu Tempuh ke Detik</h1>
+  <?php
+  $jam = 10;
+  $menit = 16;
+  $detik = 42;
+
+  $jamKeDetik = $jam * 3600;
+  $menitKeDetik = $menit * 60;
+
+  $totalDetik = $jamKeDetik + $menitKeDetik + $detik;
+
+  echo "<p>Jika waktu " . $jam . ":" . $menit . ":" . $detik . " dinyatakan dalam satuan detik adalah: " . $totalDetik . " detik.</p>";
+  ?>
+</body>
 
 </html>
